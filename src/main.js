dc53f637a870cb5c96be2087c4238c1aae773813
Ilya Obukhov <ilya.obukhov@cosysoft.ru>
2018-06-12 19:37:01 +0400

    app window on tray icon

--- src/main.js
+++ src/main.js
@@ -2,73 +2,88 @@
  * main
  */
 // Modules to control application life and create native browser window
-import { app, BrowserWindow, Menu, Tray } from 'electron';
+import { app, BrowserWindow, ipcMain, Tray } from 'electron';
 import path from 'path';
 import image from './baseline_alarm_black_18dp.png';
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
 
-let tray = null;
+let window;
+let tray;
+
+// Creates tray & window
 app.on('ready', () => {
+    createTray();
+    createWindow();
+});
+
+// Quit the app when the window is closed
+app.on('window-all-closed', () => {
+    app.quit()
+});
+
+// Creates tray image & toggles window on click
+const createTray = () => {
     const trayIconPath = path.resolve(__dirname, image);
     tray = new Tray(trayIconPath);
-    const contextMenu = Menu.buildFromTemplate([
-        { label: 'Item1', type: 'radio' },
-        { label: 'Item2', type: 'radio' },
-        { label: 'Item3', type: 'radio', checked: true },
-        { label: 'Item4', type: 'radio' }
-    ]);
-    // Menu.items.checked('is my application.');
-    tray.setToolTip('This is my application.');
-    tray.setContextMenu(contextMenu)
-});
+    tray.on('click', function (event) {
+        toggleWindow()
+    })
+};
 
-function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+const getWindowPosition = () => {
+    const windowBounds = window.getBounds();
+    const trayBounds = tray.getBounds();
+
+    // Center window horizontally below the tray icon
+    const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2));
+
+    // Position window 4 pixels vertically below the tray icon
+    const y = Math.round(trayBounds.y + trayBounds.height + 3);
+
+    return { x: x, y: y }
+};
+
+// Creates window & specifies its values
+const createWindow = () => {
+    window = new BrowserWindow({
+        width: 250,
+        height: 310,
+        show: false,
+        frame: false,
+        fullscreenable: false,
+        resizable: false,
+        transparent: true,
+        'node-integration': false
+    });
+    const indexPath = path.resolve(__dirname, 'index.html');
+    window.loadFile(indexPath);
+
+    // Hide the window when it loses focus
+    window.on('blur', () => {
+        if (!window.webContents.isDevToolsOpened()) {
+            window.hide()
+        }
     });
 
-    // and load the index.html of the app.
-    const indexPath = path.resolve(__dirname, 'index.html');
-    mainWindow.loadFile(indexPath);
-
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    window.webContents.openDevTools();
+};
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
+const toggleWindow = () => {
+    if (window.isVisible()) {
+        window.hide()
+    } else {
+        showWindow()
     }
-});
+};
 
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
-});
+const showWindow = () => {
+    const position = getWindowPosition();
+    window.setPosition(position.x, position.y, false);
+    window.show();
+    window.focus()
+};
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+ipcMain.on('show-window', () => {
+    showWindow()
+});
